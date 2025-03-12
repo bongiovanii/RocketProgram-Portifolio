0f88d3aeaf08dbe78d9c985bb14e11a8d3493d6b
Gustavo Bongiovani <guhhfive@outlook.com>
2025-03-12 14:10:04 -0300

    Update blog.js

--- blog/assets/js/blog.js
+++ blog/assets/js/blog.js
@@ -1,6 +1,9 @@
-const menuEl =  document.querySelector( "#header#dropdown-menu ");
-const menuButtonEl = document.querySelector("#header .burguer-btn ");
+const menuEl =  document.getElementById( "dropdown-menu ");
+const menuButtonEl = document.getElementsByClassName("p.burguer-btn ");
 
+
+console.log(menuEl);
+console.log(menuButtonEl);
 menuButtonEl.onclick = ()=>{
    menuEl.classList.toggle("active");
 }
