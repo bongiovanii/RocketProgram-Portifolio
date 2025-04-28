a195f47b5a1691788f983d564020cafcaf87660a
Gustavo Bongiovani <guhhfive@outlook.com>
2025-04-28 14:10:48 -0300

    Update do gerenciamento de usuários em Angular Js

--- gerenciamento-usuarios-escola/assets/js/main.js
+++ gerenciamento-usuarios-escola/assets/js/main.js
@@ -1,6 +1,34 @@
 var app = angular.module('userModule', []);
 app.controller('AppController', function ($scope) {
     $scope.mensagem = "Bem-Vindo ao sistema de cadastro Escolar";
-    $scope.user = { nome: "João", tipo: "Professor" };
+    const date = new Date();
+    const dateStr = date.toLocaleDateString();
+
+
+    $scope.filtros = {
+        aluno: false,
+        professor: false
+    };
+
+    $scope.usuarios = [
+        { nome: "João", tipo: "Professor", dataCadastro: dateStr },
+        { nome: "Maria", tipo: "Aluno", dataCadastro: dateStr },
+        { nome: "Augusto", tipo: "Aluno", dataCadastro: dateStr },
+        { nome: "Ana", tipo: "Professor", dataCadastro: dateStr },
+        { nome: "Gustavo", tipo: "Aluno", dataCadastro: dateStr },
+        { nome: "Marta", tipo: "Professor", dataCadastro: dateStr },
+    ];
+
+    $scope.filtroPorTipo = function (usuario) {
+        if (!$scope.filtros.aluno && !$scope.filtros.professor) {
+            return true;
+        }
+
+        return ($scope.filtros.aluno && usuario.tipo === 'Aluno') ||
+            ($scope.filtros.professor && usuario.tipo === 'Professor');
+
+    };
+
+
 });
 
