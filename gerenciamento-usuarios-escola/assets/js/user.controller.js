dab1ad939acae4965a298f3196c7fa977d40ff89
Gustavo Bongiovani <guhhfive@outlook.com>
2025-04-30 15:47:52 -0300

    Update sistema de gerenciamento de usuarios escola

--- gerenciamento-usuarios-escola/assets/js/user.controller.js
+++ gerenciamento-usuarios-escola/assets/js/user.controller.js
@@ -14,25 +14,45 @@ app.controller('AppController', function ($scope, UsuarioService) {
 
     $scope.userInput = {
         nome: "",
+        email: "",
         tipo: "",
         date: ""
     };
 
-    
+    $scope.loading = false;
 
-    $scope.handleSubmitAddUser = () => {
+    $scope.handleSubmitAddUser = async (form) => {
+        $scope.loading = true;
+
         const nome = $scope.userInput.nome;
+        const email = $scope.userInput.email;
         const tipo = $scope.userInput.tipo;
         const date = $scope.userInput.date;
         const dateObj = new Date(date);
         const dateStr = dateObj.toLocaleDateString();
+        const status = false;
 
-        UsuarioService.adicionarUsuario(nome, tipo, dateStr);
+        var addUser = await UsuarioService.adicionarUsuario(nome, email, tipo, dateStr, status);
+
+
+        $scope.userInput = {
+            nome: "",
+            email: "",
+            tipo: "",
+            date: ""
+        };
+
+        form.$setPristine();
+        form.$setUntouched();
+
         $scope.usuarios = UsuarioService.listar();
+        $scope.loading = false;
+
+        $scope.$apply();
 
     }
 
-    $scope.removeUsuario = (curretUsuario)=>{
+    $scope.removeUsuario = (curretUsuario) => {
         UsuarioService.removeUsuario(curretUsuario.nome);
         $scope.usuarios = UsuarioService.listar();
 
--- gerenciamento-usuarios-escola/assets/js/user.service.js
+++ gerenciamento-usuarios-escola/assets/js/user.service.js
@@ -1,26 +1,48 @@
 window.app.factory('UsuarioService', function () {
     let _usuarios = [];
 
-    const salvarUsuario = ()=> {
-        localStorage.setItem('_usuarios', JSON.stringify(_usuarios));
+    const salvarUsuario = () => {
+        localStorage.setItem('_usuarios', JSON.stringify(_usuarios.map(user => ({ nome: user.nome, email: user.email, tipo: user.tipo, dataCadastro: user.dataCadastro }))));
     }
+
+    function delay(ms) {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    async function loading(status) {
+        await delay(5000);
+        return status = true;
+    }
+
 
     return {
         listar() {
             return _usuarios;
         },
 
-        adicionarUsuario(nome, tipo, dataCadastro) {
+        async adicionarUsuario(nome, email, tipo, dataCadastro, status) {
+
             _usuarios.push({
                 nome: nome,
+                email: email,
                 tipo: tipo,
                 dataCadastro: dataCadastro
             });
-            salvarUsuario();
+
+            status = await loading(status);
+
+            if (status === true) {
+                salvarUsuario();
+                return status;
+            } else {
+                return status = false;
+            }
         },
 
-        removerUsuario(nomeUsuario){
-            _usuarios = _usuarios.filter((_usuarios)=> _usuarios.nome != nomeUsuario.nome);
+        removerUsuario(nomeUsuario) {
+            _usuarios = _usuarios.filter((_usuarios) => _usuarios.nome != nomeUsuario.nome);
             salvarUsuario();
         }
 
